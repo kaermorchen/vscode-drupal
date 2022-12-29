fae583df18afe1d14feea926c62622efd7472834
Stanislav Romanov <kaermorchen@gmail.com>
2022-12-29 13:30:39 +0300

    Add twig provider

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import { createConnection, ProposedFeatures } from 'vscode-languageserver/node';
-import HookCompletionProvider from './providers/hook-completion-provider';
+import TwigCompletionProvider from './providers/twig-completion-provider';
+// import HookCompletionProvider from './providers/hook-completion-provider';
 // import PHPCSDiagnosticProvider from './providers/phpcs-diagnostic-provider';
 
 const connection = createConnection(ProposedFeatures.all);
 
 // new PHPCSDiagnosticProvider(connection);
-new HookCompletionProvider(connection);
+// new HookCompletionProvider(connection);
+new TwigCompletionProvider(connection);
 
 connection.listen();
