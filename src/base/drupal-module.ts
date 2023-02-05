c79156b458ae9811853eb776ef65e6795f9fd90e
Stanislav Romanov <kaermorchen@gmail.com>
2023-02-05 17:12:00 +0300

    Add custom modules

--- src/base/drupal-module.ts
+++ src/base/drupal-module.ts
@@ -1,12 +1,19 @@
 import { ExtensionContext, Uri } from 'vscode';
 import Context from './context';
+import DrupalWorkspace from './drupal-workspace';
 
 export default class DrupalModule extends Context {
+  drupalWorkspace: DrupalWorkspace;
   uri: Uri;
 
-  constructor(args: { context: ExtensionContext; uri: Uri }) {
-    super(args.context);
+  constructor(arg: {
+    drupalWorkspace: DrupalWorkspace;
+    context: ExtensionContext;
+    uri: Uri;
+  }) {
+    super(arg.context);
 
-    this.uri = args.uri;
+    this.uri = arg.uri;
+    this.drupalWorkspace = arg.drupalWorkspace;
   }
 }
