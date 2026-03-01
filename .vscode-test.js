c1e8cf5f6044883c2f729e5d90c48c0489be4939
Stanislav Romanov <kaermorchen@gmail.com>
2026-03-01 11:40:20 +0300

    Add drupal-10

--- .vscode-test.js
+++ .vscode-test.js
@@ -1,12 +1,14 @@
 import { defineConfig } from '@vscode/test-cli';
 
-export default defineConfig({
-  label: 'unitTests',
-  files: 'out/**/*.test.js',
-  version: '1.100.0',
-  // workspaceFolder: './sampleWorkspace',
-  mocha: {
-    ui: 'tdd',
-    timeout: 20000,
+export default defineConfig([
+  {
+    label: 'unitTests',
+    files: 'out/**/*.test.js',
+    version: '1.100.0',
+    workspaceFolder: './testWorkspaces/drupal-10',
+    mocha: {
+      ui: 'tdd',
+      timeout: 20000,
+    },
   },
-});
+]);
